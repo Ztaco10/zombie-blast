84ebf157c73630277d8e3722d134e319eaf3fbc0
Lana Vu <lanavu@csu.fullerton.edu>
2026-04-19 19:54:59 -0700

    addition: thinking of implementing while loop in this so we dont have to handle loops in main

--- mathGame.py
+++ mathGame.py
@@ -1,11 +1,29 @@
 # im deciding to create a class to handle
 # lives, correctness, etc all in one place
+import random
 
 class mathGame():
-    def __init__(self, operation, difficulty, lives, coins, incorrect, correct):
+    def __init__(self, operation, difficulty, lives, coins, incorrect, correct, loop):
         self.operation = operation
         self.difficulty = difficulty
         self.lives = lives
         self.coins = 0
-        self.incorrect = incorrect
-        self.correct = correct
+        self.incorrect = 0
+        self.correct = 0
+        self.loop = 0
+
+    def addition(self):
+        if self.difficulty == 1:
+            while True:
+                num1 = random.randint(0,9)
+                num2 = random.randint(0,9)
+                sol = num1+num2
+                userAnswer = int(input(f"{num1} + {num2} = "))
+                if userAnswer==sol:
+                    self.correct += 1
+                else:
+                    self.incorrect += 1
+
+
+
+    
