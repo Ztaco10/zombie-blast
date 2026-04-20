06587de3b056cb8e1f42fc4183ecffc23aade3cb
Lana Vu <lanavu@csu.fullerton.edu>
2026-04-19 21:27:58 -0700

    chanegd up some self logic and addition scenes complete

--- main.py
+++ main.py
@@ -8,7 +8,18 @@ def additionScene():
             if difficultyOperation < 1 or difficultyOperation > 4:
                 raise ValueError("Error: enter a number between 1-4.")
             elif difficultyOperation == 1:
+                addition = mathGame("add", "easy")
+                addition.problem()
+            elif difficultyOperation == 2:
+                addition = mathGame("add", "medium")
+                addition.problem()
+            elif difficultyOperation == 3:
+                addition = mathGame("add", "hard")
+                addition.problem()
+        except Exception as e:
+            print(e)
                 
+
 def subtractionScene():
     difficultyOperation = int(input("1. Easy\n2. Medium\n3. Hard\n4. Return to Operations Menu\n"))
 def multiplicationScene():
