e00640371378e59955a9cdd62692b3046f8236e8
Lana Vu <lanavu@csu.fullerton.edu>
2026-04-19 21:19:26 -0700

    some code and i changed profile.py -> userprofile.py, it was showing a library name error so had to change

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import mathGame
 import login
 
+def additionScene():
+    while True:
+        try:
+            difficultyOperation = int(input("1. Easy\n2. Medium\n3. Hard\n4. Return to Operations Menu\n"))
+            if difficultyOperation < 1 or difficultyOperation > 4:
+                raise ValueError("Error: enter a number between 1-4.")
+            elif difficultyOperation == 1:
+                
+def subtractionScene():
+    difficultyOperation = int(input("1. Easy\n2. Medium\n3. Hard\n4. Return to Operations Menu\n"))
+def multiplicationScene():
+    difficultyOperation = int(input("1. Easy\n2. Medium\n3. Hard\n4. Return to Operations Menu\n"))
+
 def startScene():
     while True:
         try:
@@ -8,14 +21,15 @@ def startScene():
             if operationOption < 1 or operationOption > 4:
                 raise ValueError("Error: enter a number between 1-4.\n")
             elif operationOption == 1:
-                additionScene():
+                additionScene()
             elif operationOption == 2:
-                subtractionScene():
+                subtractionScene()
             elif operationOption == 3:
-                multiplicationScene():
+                multiplicationScene()
             elif operationOption == 4:
                 break
         except Exception as e:
             print(e)
         
 
+startScene()
