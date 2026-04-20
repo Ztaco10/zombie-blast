9f4f8c7b7cc374a1211b3259fcb74dd6961d90ff
Lana Vu <lanavu@csu.fullerton.edu>
2026-04-19 21:28:57 -0700

    sub

--- main.py
+++ main.py
@@ -18,10 +18,25 @@ def additionScene():
                 addition.problem()
         except Exception as e:
             print(e)
-                
 
 def subtractionScene():
-    difficultyOperation = int(input("1. Easy\n2. Medium\n3. Hard\n4. Return to Operations Menu\n"))
+    while True:
+        try:
+            difficultyOperation = int(input("1. Easy\n2. Medium\n3. Hard\n4. Return to Operations Menu\n"))
+            if difficultyOperation < 1 or difficultyOperation > 4:
+                raise ValueError("Error: enter a number between 1-4.")
+            elif difficultyOperation == 1:
+                sub = mathGame("sub", "easy")
+                sub.problem()
+            elif difficultyOperation == 2:
+                sub = mathGame("sub", "medium")
+                sub.problem()
+            elif difficultyOperation == 3:
+                sub = mathGame("sub", "hard")
+                sub.problem()
+        except Exception as e:
+            print(e)
+
 def multiplicationScene():
     difficultyOperation = int(input("1. Easy\n2. Medium\n3. Hard\n4. Return to Operations Menu\n"))
 
