188e2937be4a2c96c79a65002a8ae7a443d51136
Lana Vu <lanavu@csu.fullerton.edu>
2026-04-28 13:07:09 -0700

    implementing zombie im heading to lib rn

--- mathGame.py
+++ mathGame.py
@@ -1,6 +1,7 @@
 # im deciding to create a class to handle
 # lives, correctness, etc all in one place
 import random
+import json
 
 class mathGame():
     def __init__(self, operation, difficulty):
@@ -9,10 +10,23 @@ class mathGame():
         self.coins = 0
         self.incorrect = 0
         self.correct = 0
-        self.loop = 4
+        self.loop = 8
         self.lives = 3
         self.opSign = "+"
 
+    def setLives(self):
+        if self.difficulty == "easy":
+            self.lives = 5
+        elif self.difficulty == "medium":
+            self.lives = 3
+        elif self.difficulty == "hard":
+            self.lives = 2
+        return self.lives
+
+    def zombie_appears(self):
+        return random.randint(1, 100) <= 30
+    
+    
     def generateNum(self):
         if self.difficulty == "easy":
             num1 = random.randint(0,9)
@@ -31,31 +45,48 @@ class mathGame():
             while self.loop != 0:
                 num1,num2 = self.generateNum()
                 sol = num1+num2
-                try:
-                    userAnswer = int(input(f"{num1} + {num2} = "))
-                except ValueError as e:
-                    print("Error: enter a valid integer\n")
-                else:
-                    if userAnswer==sol:
+                zombie = self.zombie_appears
+                while zombie:
+                    with open ("zombie.json", "r") as f:
+                        data = json.load(f)
+                    for zombie in data:
+                        
+                    
+                    print("🧟 A zombie appeared! Solve the problem to defeat it!")
+                    try:
+                        userAnswer = int(input(f"{num1} + {num2} = "))
+                    except ValueError as e:
+                        print("Error: enter a valid integer\n")
+                    
 
-                        self.correct += 1
-                        self.coins += 10
-                        self.loop -= 1
-                        print(f"Correct! Your coins: {self.coins}\n")
-                    elif userAnswer!=sol:
-                        self.incorrect += 1
-                        #self.lives -= 1
-                        while userAnswer!= sol:
-                            print("Incorrect, try again.\n")
-                            try:
-                                userAnswer = int(input(f"{num1} + {num2} = "))
-                            except ValueError as e:
-                                print("Error: enter a valid integer\n")
-                            else:
-                                if userAnswer == sol:
-                                    print("Correct! But you don't get points for that.\n")
-                                    
-                            
+
+                else:
+                    try:
+
+                        userAnswer = int(input(f"{num1} + {num2} = "))
+                    except ValueError as e:
+                        print("Error: enter a valid integer\n")
+                    else:
+                        if userAnswer==sol:
+
+                            self.correct += 1
+                            self.coins += 10
+                            self.loop -= 1
+                            print(f"Correct! Your coins: {self.coins}\n")
+                        elif userAnswer!=sol:
+                            self.incorrect += 1
+                            #self.lives -= 1
+                            while userAnswer!= sol:
+                                print("Incorrect, try again.\n")
+                                try:
+                                    userAnswer = int(input(f"{num1} + {num2} = "))
+                                except ValueError as e:
+                                    print("Error: enter a valid integer\n")
+                                else:
+                                    if userAnswer == sol:
+                                        print("Correct! But you don't get points for that.\n")
+                                        
+                                
 
         if self.operation == "sub":
             while self.loop != 0:
