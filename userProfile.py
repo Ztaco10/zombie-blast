3e228cf4c8356fb9ffa8177c16eb00d0cefaa55a
Joshi <aidanjoshi@csu.fullerton.edu>
2026-04-28 14:10:32 -0700

    making profile menu inside a function

--- userProfile.py
+++ userProfile.py
@@ -1,5 +1,31 @@
 import login
 
+def profileMenu():
+    while(True):
+        try:
+            print('')
+            print('\033[1m' + "*** PROFILE MENU ***" + '\033[0m')
+            print("1. Account Information")
+            print("2. Change Username")
+            print("3. Change Password")
+            print("4. Exit to Main Menu ")
+
+
+            if(value != 0):
+                print('')
+                login.print_equals()
+                print('')
+            choice = int(input("Please enter the numbered option: "))
+            value = 1
+            
+            if(choice > 5 or choice < 1):
+                raise ValueError
+
+        except ValueError:
+            print('\033[1m' + "*** PLEASE ENTER A NUMBER 1-5 ***" + '\033[0m')
+            value = 0
+    return choice
+
 def total_currency():
     return None
 
@@ -14,6 +40,7 @@ login.print_equals()
 print('')
 
 value = 0
+
 
 while(True):
     try:
@@ -38,4 +65,6 @@ while(True):
     except ValueError:
         print('\033[1m' + "*** PLEASE ENTER A NUMBER 1-5 ***" + '\033[0m')
         value = 0
+    
+    if()
         
